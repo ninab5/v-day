f48c12a604d157703c615cb4a026a0276afc1059
ninab5 <nina.b5@icloud.com>
2025-02-09 16:56:31 +0100

    Delete btnscript.js

--- btnscript.js
+++
@@ -1,6 +0,0 @@
-$('button').on('mouseover', function() {
-    $(this).animate({
-      'top': Math.random() * window.innerHeight + "px",
-      'left': Math.random() * window.innerWidth + "px"
-    }, "medium");
-  })
