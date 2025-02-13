661e06fb9e1effaa47d8b2459ce04947160637ba
ninab5 <nina.b5@icloud.com>
2025-02-13 22:47:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   const envelope = document.querySelector('.envelope-wrapper');
   const heart = document.querySelector('.heart');
+  const hearts = document.querySelector('.hearts');
+
+  hearts.classList.remove('start-heart-animation');
   
   envelope.addEventListener('click', () => {
     envelope.classList.toggle('flap');
-    heart.classList.toggle('flap'); // Toggling the 'flap' class on heart as well
+    hearts.classList.toggle('flap');
   });
 });
